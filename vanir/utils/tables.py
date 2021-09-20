6b68f6df464922f8f7c23636024204105cf17211
guanana <guanana2@gmail.com>
2021-09-21 00:48:30 +0100

    Table link defaults to detail instead of edit

--- vanir/utils/tables.py
+++ vanir/utils/tables.py
@@ -2,9 +2,10 @@ import django_tables2 as tables
 
 
 class ObjectTable(tables.Table):
-    name = tables.TemplateColumn('<a href="{{record.get_absolute_url}}edit/">{{record.name}}</a>')
+    name = tables.TemplateColumn(
+        '<a href="{{record.get_absolute_url}}">{{record.name}}</a>'
+    )
 
     class Meta:
         model = None
         template_name = "django_tables2/bootstrap.html"
-
