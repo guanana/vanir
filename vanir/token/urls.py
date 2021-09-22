a451d073908b214f948a053144792f55944e2f37
guanana <guanana2@gmail.com>
2021-09-22 23:22:14 +0100

    Token update value and update bulk views and add to detail

--- vanir/token/urls.py
+++ vanir/token/urls.py
@@ -1,12 +1,30 @@
 from django.urls import path
 
-from .views import TokenListView, TokenUpdateView, TokenDetailView, TokenDeleteView, TokenCreateView
+from .views import (
+    TokenBulkUpdateValueView,
+    TokenCreateView,
+    TokenDeleteView,
+    TokenDetailUpdateValueView,
+    TokenDetailView,
+    TokenListView,
+    TokenUpdateView,
+)
 
 app_name = "token"
 urlpatterns = [
-        path('', TokenListView.as_view(), name="token_list"),
-        path('add/', TokenCreateView.as_view(), name="token_add"),
-        path('<int:pk>/', TokenDetailView.as_view(), name='token_detail'),
-        path('<int:pk>/edit/', TokenUpdateView.as_view(), name='token_update'),
-        path('<int:pk>/delete/', TokenDeleteView.as_view(), name='token_delete'),
+    path("", TokenListView.as_view(), name="token_list"),
+    path("add/", TokenCreateView.as_view(), name="token_add"),
+    path(
+        "bulk_update_price/",
+        TokenBulkUpdateValueView.as_view(),
+        name="token_bulk_update_price",
+    ),
+    path("<int:pk>/", TokenDetailView.as_view(), name="token_detail"),
+    path("<int:pk>/edit/", TokenUpdateView.as_view(), name="token_update"),
+    path("<int:pk>/delete/", TokenDeleteView.as_view(), name="token_delete"),
+    path(
+        "<int:pk>/update_price/",
+        TokenDetailUpdateValueView.as_view(),
+        name="token_update_price",
+    ),
 ]
--- vanir/token/views.py
+++ vanir/token/views.py
@@ -2,12 +2,18 @@ from django.urls import reverse_lazy
 
 from vanir.token.models import Token
 from vanir.token.tables import TokenTable
-from vanir.utils.views import ObjectListView, ObjectUpdateView, ObjectDetailView, ObjectDeleteView, ObjectCreateView
+from vanir.utils.views import (
+    ObjectCreateView,
+    ObjectDeleteView,
+    ObjectDetailView,
+    ObjectListView,
+    ObjectUpdateView,
+)
 
 
 class TokenCreateView(ObjectCreateView):
     model = Token
-    fields = ("name", "symbol", "blockchain", "decimals")
+    fields = ("name", "symbol", "blockchain", "last_value")
 
 
 class TokenListView(ObjectListView):
@@ -26,3 +32,26 @@ class TokenDetailView(ObjectDetailView):
 class TokenDeleteView(ObjectDeleteView):
     model = Token
     success_url = reverse_lazy("token:token_list")
+
+
+class TokenDetailUpdateValueView(ObjectDetailView):
+    model = Token
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.get_value()
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
+
+
+class TokenBulkUpdateValueView(ObjectListView):
+    model = Token
+    table_class = TokenTable
+
+    def get(self, request, *args, **kwargs):
+        for token in Token.objects.all():
+            try:
+                token.get_value()
+            except ValueError:
+                pass
+        return super(TokenBulkUpdateValueView, self).get(request, *args, **kwargs)
