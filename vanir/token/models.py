d56ee72adacb322c19c3a051fcaaee135b342c13
guanana <guanana2@gmail.com>
2021-09-22 23:19:39 +0100

    Remove decimal field in token and add method to retrieve value

--- vanir/token/models.py
+++ vanir/token/models.py
@@ -1,3 +1,4 @@
+from binance.exceptions import BinanceAPIException
 from django.db import models
 
 from vanir.blockchain.models import Blockchain
@@ -9,14 +10,45 @@ class Coin(BaseObject):
     name = models.CharField(max_length=40)
     symbol = models.CharField(max_length=6)
     blockchain = models.ForeignKey(Blockchain, on_delete=models.CASCADE)
-    decimals = models.IntegerField(default=10)
+    last_value = models.DecimalField(decimal_places=10, max_digits=40, null=True)
 
     def __str__(self):
         return self.symbol
 
     def get_absolute_url(self):
         from django.urls import reverse
-        return reverse('token:coin_detail', kwargs={'pk': self.pk})
+
+        return reverse("token:coin_detail", kwargs={"pk": self.pk})
+
+    def get_value(self, account=None):
+        if not account:
+            from vanir.utils.helpers import fetch_default_account
+
+            account = fetch_default_account()
+
+        # Special exception when calling same pair
+        if self.symbol == account.token_pair:
+            return 0
+
+        from vanir.account.utils import get_exchange
+
+        exchange_obj = get_exchange(account.pk)
+        try:
+            price = exchange_obj.con.get_avg_price(
+                symbol=f"{self.symbol}{account.token_pair}"
+            )["price"]
+        except BinanceAPIException as binanceexception:
+            if binanceexception.code == -1121:
+                raise ValueError(
+                    f"Pair {self.symbol}{account.token_pair} not supported"
+                )
+
+        try:
+            self.last_value = float(price)
+        except ValueError:
+            raise ValueError("Something went wrong, try again later")
+        self.save()
+        return self.last_value
 
 
 class Token(Coin):
@@ -24,4 +56,5 @@ class Token(Coin):
 
     def get_absolute_url(self):
         from django.urls import reverse
-        return reverse('token:token_detail', kwargs={'pk': self.pk})
+
+        return reverse("token:token_detail", kwargs={"pk": self.pk})
