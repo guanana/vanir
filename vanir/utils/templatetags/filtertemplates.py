467b8d39b1f0b28f3363d753cd2dc2493c915fa1
guanana <guanana2@gmail.com>
2021-09-22 01:50:39 +0100

    Add some more filters to make it easier to parse urls and images and simplify view

--- vanir/utils/templatetags/filtertemplates.py
+++ vanir/utils/templatetags/filtertemplates.py
@@ -1,6 +1,10 @@
 import json
+import os
 
 from django import template
+from django.conf import settings
+from django.templatetags.static import static as templatetags_static
+from django.urls import NoReverseMatch, reverse
 
 register = template.Library()
 
@@ -13,3 +17,50 @@ def add_class(field, class_name):
 @register.filter
 def pretty_json(value):
     return json.dumps(value, indent=4)
+
+
+@register.filter()
+def validated_viewname(model, action):
+    """
+    Return the view name for the given model and action if valid, or None if invalid.
+    """
+    viewname = f"{model._meta.app_label}:{model._meta.model_name}_{action}"
+    try:
+        # Validate and return the view name. We don't return the actual URL yet because many of the templates
+        # are written to pass a name to {% url %}.
+        reverse(viewname)
+        return viewname
+    except NoReverseMatch:
+        return None
+
+
+@register.filter()
+def get_viewname(model_name: str, action: str):
+    viewname = f"{model_name.lower()}:{model_name.lower()}_{action}"
+    try:
+        # Validate and return the view name. We don't return the actual URL yet because many of the templates
+        # are written to pass a name to {% url %}.
+        reverse(viewname)
+        return viewname
+    except NoReverseMatch:
+        return None
+
+
+@register.filter()
+def get_model_image(model_name, image_name: str = ""):
+    path_name = (
+        f"images/{model_name}/{model_name}.png"
+        if not image_name
+        else f"images/{model_name}/{image_name}"
+    )
+    if settings.DEBUG:
+        if os.path.exists(f"{settings.APPS_DIR}{templatetags_static(path_name)}"):
+            return path_name
+        else:
+            return None
+    return path_name
+
+
+@register.filter()
+def get_model_name(model):
+    return model._meta.model_name
--- vanir/utils/views.py
+++ vanir/utils/views.py
@@ -10,11 +10,6 @@ class ObjectCreateView(CreateView):
     model = None
     template_name = "object_form.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["model_name"] = self.model.__name__
-        return context
-
 
 @method_decorator(login_required, name="dispatch")
 class ObjectListView(SingleTableView):
