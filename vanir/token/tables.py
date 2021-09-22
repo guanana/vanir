dcc700fabf43c96a42e3df964cb636f1c0de9b3a
guanana <guanana2@gmail.com>
2021-09-22 23:21:12 +0100

    Change last_value print in TokenTable and replace decimal non existent field with last_value

--- vanir/token/tables.py
+++ vanir/token/tables.py
@@ -1,10 +1,17 @@
-import django_tables2 as tables
-from .models import Token
+from django_tables2 import Column
+
 from vanir.utils.tables import ObjectTable
+
+from ..utils.helpers import fetch_default_account
+from .models import Token
 
 
 class TokenTable(ObjectTable):
+    last_value = Column(
+        verbose_name=f"Last value in {fetch_default_account().token_pair}"
+    )
+
     class Meta:
         model = Token
         template_name = "django_tables2/bootstrap.html"
-        fields = ("name", "symbol", "blockchain", "decimals")
+        fields = ("name", "symbol", "blockchain", "last_value")
