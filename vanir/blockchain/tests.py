3cba955b45c7f7c64a5d5839a693e2a39f457006
guanana <guanana2@gmail.com>
2021-09-24 21:28:42 +0100

    Add some basic tests on blockchain

--- vanir/blockchain/tests.py
+++ vanir/blockchain/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+from vanir.blockchain.models import Blockchain
+
+
+class BlockchainModelTest(TestCase):
+    def setUp(self):
+        self.blockchain = Blockchain.objects.create(name="TestBlockchain")
+
+    def test_createblockchain(self):
+        blockchain = Blockchain.objects.get(name="TestBlockchain")
+        self.assertEqual(self.blockchain, blockchain)
