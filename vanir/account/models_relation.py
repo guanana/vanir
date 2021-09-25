44edb3ad4198609dd2ba0e610778a3687cfc347d
guanana <guanana2@gmail.com>
2021-09-26 00:08:04 +0100

    Update time change from updated when created the model entry to only when save (to be able to update instead of create new objects every time)

--- vanir/account/models_relation.py
+++ vanir/account/models_relation.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 from vanir.token.models import Token
@@ -11,4 +13,11 @@ class AccountTokens(models.Model):
     account = models.ForeignKey("Account", on_delete=models.CASCADE)
     token = models.ForeignKey(Token, on_delete=models.CASCADE)
     quantity = models.FloatField(default=0)
-    update_time = models.TimeField(auto_now=True)
+    update_time = models.TimeField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        token = AccountTokens.objects.filter(token=self.token)
+        if token:
+            self.update_time = datetime.now()
+        else:
+            super().save(*args, **kwargs)
