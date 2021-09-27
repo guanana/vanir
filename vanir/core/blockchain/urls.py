b9ef62273fb89bde71c821619c7e65a805d8c0c0
guanana <guanana2@gmail.com>
2021-09-27 03:58:14 +0100

    Move structure from "plain" to "core" and the rest Core --> Account, Blockchain, Exchange and Token

---
+++ vanir/core/blockchain/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from .views import (
+    BlockchainCreateView,
+    BlockchainDeleteView,
+    BlockchainDetailView,
+    BlockchainListView,
+    BlockchainUpdateView,
+)
+
+app_name = "blockchain"
+urlpatterns = [
+    path("", BlockchainListView.as_view(), name="blockchain_list"),
+    path("add/", BlockchainCreateView.as_view(), name="blockchain_add"),
+    path("<int:pk>/", BlockchainDetailView.as_view(), name="blockchain_detail"),
+    path("<int:pk>/edit/", BlockchainUpdateView.as_view(), name="blockchain_update"),
+    path("<int:pk>/delete/", BlockchainDeleteView.as_view(), name="blockchain_delete"),
+]
---
+++ vanir/core/exchange/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from .views import (
+    ExchangeCreateView,
+    ExchangeDeleteView,
+    ExchangeDetailView,
+    ExchangeListView,
+    ExchangeUpdateView,
+)
+
+app_name = "exchange"
+urlpatterns = [
+    path("", ExchangeListView.as_view(), name="exchange_list"),
+    path("add/", ExchangeCreateView.as_view(), name="exchange_add"),
+    path("<int:pk>/", ExchangeDetailView.as_view(), name="exchange_detail"),
+    path("<int:pk>/edit/", ExchangeUpdateView.as_view(), name="exchange_update"),
+    path("<int:pk>/delete/", ExchangeDeleteView.as_view(), name="exchange_delete"),
+]
