18d184a08f5525c3bb5d87a461c842dbb020dcec
guanana <guanana2@gmail.com>
2021-09-27 03:38:08 +0100

    Fix bug with account<->token relationship not being updated properly

--- vanir/token/helpers/import_utils.py
+++ vanir/token/helpers/import_utils.py
@@ -1,3 +1,6 @@
+import logging
+
+from django.core.exceptions import ValidationError
 from django.db.models import QuerySet
 
 from vanir.account.models import Account
@@ -5,6 +8,8 @@ from vanir.account.models_relation import AccountTokens
 from vanir.blockchain.models import Blockchain
 from vanir.token.models import Token
 from vanir.utils.helpers import fetch_default_account, value_pair
+
+logger = logging.getLogger(__name__)
 
 
 def get_token_full_name(account: Account, token_symbol: str) -> str:
@@ -19,15 +24,37 @@ def get_token_full_name(account: Account, token_symbol: str) -> str:
 
 
 def token_import(
-    account: Account, token: str, quantity: float = None, blockchain: Blockchain = None
+    account: Account,
+    token_symbol: str,
+    token_fullname: str = None,
+    quantity: float = None,
+    blockchain: Blockchain = None,
 ) -> Token:
     if not blockchain:
         blockchain = account.exchange_obj.default_blockchain
-    token_obj, token_created = Token.objects.get_or_create(
-        name=get_token_full_name(account, token), symbol=token, blockchain=blockchain
-    )
+    try:
+        if not token_fullname:
+            token_fullname = get_token_full_name(account, token_symbol)
+        token_obj = Token.objects.get(symbol=token_symbol)
+        if token_obj.name != token_fullname and not account.testnet:
+            logger.info(
+                f"Updating token name from {token_obj.name} to {token_fullname}"
+            )
+            token_obj.name = token_fullname
+            token_obj.save()
+    except Token.DoesNotExist:
+        token_obj = Token.objects.create(
+            name=token_fullname, symbol=token_symbol, blockchain=blockchain
+        )
     if quantity:
-        AccountTokens(account=account, token=token_obj, quantity=quantity).save()
+        try:
+            account_token = AccountTokens.objects.get(account=account, token=token_obj)
+        except AccountTokens.DoesNotExist:
+            account_token = AccountTokens.objects.create(
+                account=account, token=token_obj
+            )
+        account_token.quantity = quantity
+        account_token.save()
     return token_obj
 
 
@@ -53,6 +80,8 @@ def qs_update(token_qs: QuerySet[Token], account: Account = None):
 def bulk_update(account: Account = None):
     if not account:
         account = fetch_default_account()
+        if not account:
+            raise ValidationError("You need to have at least one account configured")
     price_dict = account.exchange_obj.all_assets_prices
     for token_obj in Token.objects.all():
         token_update(token_obj, account, price_dict)
