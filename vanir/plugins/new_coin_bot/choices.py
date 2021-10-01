f013d3afef90ef5a04ba0f61fcc739e62bb03ce8
guanana <guanana2@gmail.com>
2021-10-01 02:23:57 +0100

    Add schedule choice model

--- vanir/plugins/new_coin_bot/choices.py
+++ vanir/plugins/new_coin_bot/choices.py
@@ -5,3 +5,15 @@ class DiscoverMethod(models.TextChoices):
     BINANCE_SCRAPPER = "Binance Scrapper"
     MANUAL = "Manual"
     OTHER = "Other"
+
+
+class ScheduleScrap(models.IntegerChoices):
+    EVERY_5_MINS = "5"
+    EVERY_10_MINS = "10"
+    EVERY_15_MINS = "15"
+    EVERY_20_MINS = "20"
+    EVERY_30_MINS = "30"
+    EVERY_50_MINS = "50"
+    EVERY_1_HOUR = "60"
+    EVERY_1_AND_HALF_HOURS = "90"
+    EVERY_2_HOURS = "120"
