dd30971371318f80e81a9b03835f26cb20e91872
guanana <guanana2@gmail.com>
2021-10-01 02:27:12 +0100

    Add new class for timestamped models NewCoinModel inheritance change, add fields

--- vanir/plugins/new_coin_bot/models.py
+++ vanir/plugins/new_coin_bot/models.py
@@ -2,9 +2,40 @@ from django.db import models
 
 from vanir.core.token.models import Token
 from vanir.plugins.models import PluginBase
-from vanir.plugins.new_coin_bot.choices import DiscoverMethod
+from vanir.plugins.new_coin_bot.choices import DiscoverMethod, ScheduleScrap
+from vanir.utils.models import TimeStampedMixin
 
 
-class NewCoinModel(Token, PluginBase):
+class NewCoinModel(PluginBase, Token, TimeStampedMixin):
     discovered_method = models.CharField(max_length=25, choices=DiscoverMethod.choices)
-    listing_day = models.DateTimeField()
+    listing_day = models.DateTimeField(null=True)
+    announcement_seen = models.IntegerField(null=True)
+    scrapping_interval = models.IntegerField(
+        max_length=4, choices=ScheduleScrap, default=10
+    )
+
+    class Meta:
+        abstract = True
+
+    def increase_announcement_seen(self):
+        self.announcement_seen += 1
+        self.save()
+
+    @property
+    def is_new(self):
+        if self.announcement_seen <= 1:
+            return True
+        return False
+
+    def promote_to_standard_token(self):
+        """
+        Creates a token in the core DB and removes itself from the NewCoinModel
+        """
+        Token.objects.create(name=self.name, symbol=self.symbol)
+        self.delete()
+
+
+class BinanceNewTokenModel(NewCoinModel):
+    discovered_method = models.CharField(
+        max_length=25, choices=DiscoverMethod.choices, default="Binance Scrapper"
+    )
--- vanir/utils/models.py
+++ vanir/utils/models.py
@@ -24,3 +24,11 @@ class BaseObject(models.Model):
 
         class_name = self.__name__.lower()
         return reverse(f"{class_name}:{class_name}_add")
+
+
+class TimeStampedMixin(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
