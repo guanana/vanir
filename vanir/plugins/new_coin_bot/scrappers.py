fb8cc70a46fa870ef44a3454fa4c750e94ed17e6
guanana <guanana2@gmail.com>
2021-10-01 02:27:22 +0100

    Add BaseScrap class and AnnouncementScrap to inherit from Each match now adds line to a list, that list is used to then trace back the URLs

--- vanir/plugins/new_coin_bot/scrappers.py
+++ vanir/plugins/new_coin_bot/scrappers.py
@@ -3,8 +3,17 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+from .choices import DiscoverMethod
+from .helpers import token_already_exists
+from .models import BinanceNewTokenModel
+
 
 class BaseScrap:
+    def __init__(self, scrap_option: str):
+        self.scrap_option = scrap_option
+        self.url = ""
+        self.match_lines = dict()
+
     SCRAP_URLS = {
         "Binance": {
             "base_url": "https://www.binance.com",
@@ -12,23 +21,7 @@ class BaseScrap:
             "scrap_pattern": re.compile("link-0-"),
         }
     }
-    URL_HREF_PATTERN = re.compile("href=(?P<url>)")
-    LIST_PATTERN = re.compile("List .* (?P<symbol>\((\w+\)))")
-    PAIR_PATTERN = re.compile("(?P<pair>\w+/\w+)")
 
-
-class ScrapBinance(BaseScrap):
-    def __init__(self, scrap_option: str):
-        self.base_url = self.SCRAP_URLS.get(scrap_option)["base_url"]
-        self.url = (
-            f'{self.base_url}{self.SCRAP_URLS.get(scrap_option)["announcement_path"]}'
-        )
-        self.url_pattern = self.SCRAP_URLS.get(scrap_option)["scrap_pattern"]
-        if not self.url:
-            raise ValueError(f"{scrap_option} not a valid option")
-        self.link_content = {}
-
-    @property
     def _raw_content(self):
         page = requests.get(self.url)
         if not page:
@@ -36,21 +29,59 @@ class ScrapBinance(BaseScrap):
         soup = BeautifulSoup(page.content, "html.parser")
         return soup
 
+
+class AnnouncementScrap(BaseScrap):
+    LIST_PATTERN = re.compile("List .* (?P<symbol>\((\w+\)))")  # noqa W605
+    PAIR_PATTERN = re.compile("(?P<pair>\w+/\w+)")  # noqa W605
+
+    @property
+    def _last_token_announcements(self):
+        """
+        Aggregation method that will join all list of tokens and return just
+        a unique list of tokens to import
+        @returns: list(symbols: str)
+        """
+        raise NotImplementedError
+
+    def remove_existing_tokens(self):
+        """
+        Checks against existing tokens in core Token DB and remove
+        from the list the ones are already present
+        """
+        raise NotImplementedError
+
+    def import_token_announcements(self):
+        """
+        Add into model the tokens that are really new
+        """
+        raise NotImplementedError
+
+
+class ScrapBinance(AnnouncementScrap):
+    DISCOVER_METHOD = DiscoverMethod.BINANCE_SCRAPPER
+    URL_HREF_PATTERN = re.compile("href=(?P<url>)")  # noqa W605
+
+    def __init__(self, scrap_option: str = "Binance"):
+        super().__init__(scrap_option)
+
+        self.base_url = self.SCRAP_URLS.get(self.scrap_option)["base_url"]
+        self.url = f'{self.base_url}{self.SCRAP_URLS.get(self.scrap_option)["announcement_path"]}'
+        self.url_pattern = self.SCRAP_URLS.get(self.scrap_option)["scrap_pattern"]
+        if not self.url:
+            raise ValueError(f"{self.scrap_option} not a valid option")
+        self.link_content = {}
+
     @property
     def text_content(self):
-        content = self._raw_content.findAll("a", id=self.url_pattern)
+        content = self._raw_content().findAll("a", id=self.url_pattern)
         return [item.text for item in content]
 
     @property
-    def url_content(self):
-        content = self._raw_content.find_all("a", id=self.url_pattern)
-        dict_urls = {item.text: f"{self.base_url}{item['href']}" for item in content}
-        temp_dict = dict_urls.copy()
-        for text, url in temp_dict.items():
-            match_list = self.LIST_PATTERN.search(text)
-            match_pair = self.PAIR_PATTERN.findall(text)
-            if not match_list and not match_pair:
-                dict_urls.pop(text)
+    def url_lines(self):
+        content = self._raw_content().find_all("a", id=self.url_pattern)
+        dict_urls = {
+            idx: f"{self.base_url}{item['href']}" for idx, item in enumerate(content)
+        }
         return dict_urls
 
     @property
@@ -59,11 +90,13 @@ class ScrapBinance(BaseScrap):
         Scrapes new listings page for and returns new Symbol when appropriate
         """
         direct_list_tokens = []
-        for line in self.text_content:
+        for idx, line in enumerate(self.text_content):
             match_pattern = self.LIST_PATTERN.search(line)
             if match_pattern:
                 try:
-                    direct_list_tokens.append(match_pattern.group("symbol")[1:-1])
+                    symbol = match_pattern.group("symbol")[1:-1]
+                    direct_list_tokens.append(symbol)
+                    self.match_lines.update({symbol: idx})
                 except AttributeError:
                     pass
         return direct_list_tokens
@@ -74,7 +107,7 @@ class ScrapBinance(BaseScrap):
         Scrapes new pairs and returns new pairs when appropriate
         """
         new_pair_tokens = []
-        for line in self.text_content:
+        for idx, line in enumerate(self.text_content):
             match_pattern = self.PAIR_PATTERN.findall(line)
             if match_pattern:
                 for pair in match_pattern:
@@ -82,7 +115,32 @@ class ScrapBinance(BaseScrap):
                     symbol2 = pair.split("/")[1]
                     new_pair_tokens.append(symbol1)
                     new_pair_tokens.append(symbol2)
+                    self.match_lines[symbol1] = idx
+                    self.match_lines[symbol2] = idx
         return new_pair_tokens
 
-    def get_last_coins(self):
+    @property
+    def _last_token_announcements(self):
         return self.direct_list_tokens + self.new_pair_tokens
+
+    def remove_existing_tokens(self):
+        for token_symbol in self._last_token_announcements:
+            if token_already_exists(token_symbol):
+                self._last_token_announcements.pop()
+
+    def import_token_announcements(self):
+        self.remove_existing_tokens()
+        for token_symbol in self._last_token_announcements:
+            new_coin = BinanceNewTokenModel.objects.get_or_create(
+                name=token_symbol,
+                symbol=token_symbol,
+                discovered_method=self.DISCOVER_METHOD,
+            )
+            new_coin.increase_announcement_seen()
+
+    def follow_urls(self, symbol):
+        if len(self._last_token_announcements) > 0:
+            try:
+                return self.url_lines[self.match_lines[symbol]]
+            except KeyError:
+                pass
