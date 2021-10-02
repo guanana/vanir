00d4c9ac191a31b2bd4b0bd717e660e3361adb7f
guanana <guanana2@gmail.com>
2021-10-02 21:32:26 +0100

    Add first views to new_coin_bot

--- vanir/plugins/new_coin_bot/views.py
+++ vanir/plugins/new_coin_bot/views.py
@@ -1,3 +1,8 @@
-# from django.shortcuts import render
+from vanir.plugins.new_coin_bot.models import BinanceNewToken
+from vanir.plugins.new_coin_bot.tables import BinanceNewTokenTable
+from vanir.plugins.views import PluginListView
 
-# Create your views here.
+
+class BinanceNewTokenListView(PluginListView):
+    model = BinanceNewToken
+    table_class = BinanceNewTokenTable
--- vanir/plugins/views.py
+++ vanir/plugins/views.py
@@ -1,3 +1,13 @@
-# from django.shortcuts import render
+from vanir.utils.views import ObjectListView, set_context_data
 
-# Create your views here.
+
+class PluginListView(ObjectListView):
+    model = None
+    table_class = None
+    template_name = "object_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["plugin"] = True
+        context["title"] = f" plugin {context['model_name']}"
+        return set_context_data(context, self)
