ebf21b26b520bd726874ee4ef0fe2721842f2ad7
guanana <guanana2@gmail.com>
2021-10-02 21:33:10 +0100

    Modify inherited get_absolut_url for plugins

--- vanir/plugins/models.py
+++ vanir/plugins/models.py
@@ -1,11 +1,15 @@
-from django.db import models
-
 from vanir.utils.models import BaseObject
 
 
 class PluginBase(BaseObject):
-    name = models.CharField(max_length=250)
-
     class Meta:
         abstract = True
         app_label = "plugins"
+
+    def get_absolute_url(self):
+        from django.urls import reverse
+
+        class_name = self.__class__.__name__.lower()
+        return reverse(
+            f"plugins:{class_name}:{class_name}_detail", kwargs={"pk": self.pk}
+        )
