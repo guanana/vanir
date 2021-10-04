81ee31dd69fccba58eacef95c51f89c819ccbffd
guanana <guanana2@gmail.com>
2021-10-04 03:31:48 +0100

    Register models for admin

--- vanir/plugins/new_coin_bot/admin.py
+++ vanir/plugins/new_coin_bot/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 
 # Register your models here.
+from vanir.plugins.new_coin_bot.models import BinanceNewToken, NewCoinConfig
+
+admin.site.register(NewCoinConfig)
+admin.site.register(BinanceNewToken)
