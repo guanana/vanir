4413d181b8efb84d4913d34cfd809da283c70301
guanana <guanana2@gmail.com>
2021-10-04 03:33:11 +0100

    Make choices Int and change scapper name to shared task name

--- vanir/plugins/new_coin_bot/choices.py
+++ vanir/plugins/new_coin_bot/choices.py
@@ -7,13 +7,18 @@ class DiscoverMethod(models.TextChoices):
     OTHER = "Other"
 
 
+class ScrapperOptions(models.TextChoices):
+    BINANCE_SCRAPPER = "ScrapBinanceModelWithDate"
+
+
 class ScheduleScrap(models.IntegerChoices):
-    EVERY_5_MINS = "5"
-    EVERY_10_MINS = "10"
-    EVERY_15_MINS = "15"
-    EVERY_20_MINS = "20"
-    EVERY_30_MINS = "30"
-    EVERY_50_MINS = "50"
-    EVERY_1_HOUR = "60"
-    EVERY_1_AND_HALF_HOURS = "90"
-    EVERY_2_HOURS = "120"
+    EVERY_2_MINS = 2
+    EVERY_5_MINS = 5
+    EVERY_10_MINS = 10
+    EVERY_15_MINS = 15
+    EVERY_20_MINS = 20
+    EVERY_30_MINS = 30
+    EVERY_50_MINS = 50
+    EVERY_1_HOUR = 60
+    EVERY_1_AND_HALF_HOURS = 90
+    EVERY_2_HOURS = 120
