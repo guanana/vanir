d3496aa2e08726f19717e9046e4c268dfa96f69e
guanana <guanana2@gmail.com>
2021-10-04 03:45:52 +0100

    Add new urls and views for the plugin

--- vanir/plugins/new_coin_bot/views.py
+++ vanir/plugins/new_coin_bot/views.py
@@ -1,8 +1,95 @@
-from vanir.plugins.new_coin_bot.models import BinanceNewToken
-from vanir.plugins.new_coin_bot.tables import BinanceNewTokenTable
-from vanir.plugins.views import PluginListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import redirect
+from django.urls import reverse, reverse_lazy
+
+from vanir.plugins.new_coin_bot.models import (
+    BinanceNewToken,
+    NewCoinConfig,
+    ScrapBinanceModel,
+)
+from vanir.plugins.new_coin_bot.tables import BinanceNewTokenTable, NewCoinConfigTable
+from vanir.plugins.views import (
+    PluginCreateView,
+    PluginDeleteView,
+    PluginDetailView,
+    PluginListView,
+    PluginUpdateView,
+)
+
+
+# New Coin Config
+class NewCoinConfigCreateView(PluginCreateView):
+    model = NewCoinConfig
+    fields = (
+        "scrapper_class_name",
+        "scrapping_interval",
+        "activate_scheduler",
+        "auto_clean",
+    )
+
+
+class NewCoinConfigListView(PluginListView):
+    model = NewCoinConfig
+    table_class = NewCoinConfigTable
+
+
+class NewCoinConfigUpdateView(PluginUpdateView):
+    model = NewCoinConfig
+    fields = ("scrapping_interval", "activate_scheduler", "auto_clean")
+
+
+class NewCoinConfigDetailView(PluginDetailView):
+    model = NewCoinConfig
+
+
+class NewCoinConfigDeleteView(PluginDeleteView):
+    model = NewCoinConfig
+    success_url = reverse_lazy("plugins:new_coin_bot:newcoinconfig_list")
+
+
+# Binance New Token
+class BinanceNewTokenCreateView(PluginCreateView):
+    model = BinanceNewToken
+    fields = ("name", "symbol", "discovered_method", "listing_day")
 
 
 class BinanceNewTokenListView(PluginListView):
     model = BinanceNewToken
     table_class = BinanceNewTokenTable
+
+
+class BinanceNewTokenUpdateView(PluginUpdateView):
+    model = BinanceNewToken
+    fields = (
+        "name",
+        "discovered_method",
+        "listing_day",
+    )
+
+
+class BinanceNewTokenDetailView(PluginDetailView):
+    model = BinanceNewToken
+
+
+class BinanceNewTokenDeleteView(PluginDeleteView):
+    model = BinanceNewToken
+    success_url = reverse_lazy("plugins:new_coin_bot:binancenewtoken_list")
+
+
+class BinanceNewTokenUpdateValueView(PluginListView, SuccessMessageMixin):
+    model = BinanceNewToken
+    table_class = BinanceNewTokenTable
+    success_message = "Scrap completed"
+
+    def get(self, request, *args, **kwargs):
+        ScrapBinanceModel().run()
+        return redirect(reverse("plugins:new_coin_bot:binancenewtoken_list"))
+
+
+class BinanceNewTokenPromoteView(PluginDetailView):
+    model = BinanceNewToken
+
+    def get(self, request, *args, **kwargs):
+        super().get(request, *args, **kwargs)
+        token = self.object.promote_to_standard_token()
+        return redirect(reverse("token:token_detail", kwargs={"pk": token.pk}))
--- vanir/plugins/views.py
+++ vanir/plugins/views.py
@@ -1,11 +1,53 @@
-from vanir.utils.views import ObjectListView, set_context_data
+from vanir.utils.views import (
+    ObjectCreateView,
+    ObjectDeleteView,
+    ObjectDetailView,
+    ObjectListFilterView,
+    ObjectListView,
+    ObjectUpdateView,
+    set_context_data,
+)
+
+
+class PluginCreateView(ObjectCreateView):
+    model = None
+    template_name = "object_form.html"
 
 
 class PluginListView(ObjectListView):
-    model = None
-    table_class = None
-    template_name = "object_list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["plugin"] = True
+        context["title"] = f" plugin {context['model_name']}"
+        return set_context_data(context, self)
 
+
+class PluginListFilterView(ObjectListFilterView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["plugin"] = True
+        context["title"] = f" plugin {context['model_name']}"
+        return set_context_data(context, self)
+
+
+class PluginUpdateView(ObjectUpdateView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["plugin"] = True
+        context["title"] = f" plugin {context['model_name']}"
+        return set_context_data(context, self)
+
+
+class PluginDetailView(ObjectDetailView):
+    model = None
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["plugin"] = True
+        return set_context_data(context, self)
+
+
+class PluginDeleteView(ObjectDeleteView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["plugin"] = True
