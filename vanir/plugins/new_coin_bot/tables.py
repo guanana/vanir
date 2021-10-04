db18e0b98e77e9d5d1a2ad6132513ac98f68dee1
guanana <guanana2@gmail.com>
2021-10-04 03:45:11 +0100

    Add NewCoinConfigTable

--- vanir/plugins/new_coin_bot/tables.py
+++ vanir/plugins/new_coin_bot/tables.py
@@ -1,4 +1,4 @@
-from vanir.plugins.new_coin_bot.models import BinanceNewToken
+from vanir.plugins.new_coin_bot.models import BinanceNewToken, NewCoinConfig
 from vanir.utils.tables import ObjectTable
 
 
@@ -6,3 +6,25 @@ class BinanceNewTokenTable(ObjectTable):
     class Meta:
         model = BinanceNewToken
         template_name = "django_tables2/bootstrap.html"
+        fields = (
+            "name",
+            "created_on",
+            "updated",
+            "symbol",
+            "listing_day",
+            "discovered_method",
+        )
+
+
+class NewCoinConfigTable(ObjectTable):
+    class Meta:
+        model = NewCoinConfig
+        template_name = "django_tables2/bootstrap.html"
+        fields = (
+            "name",
+            "created_on",
+            "updated",
+            "scrapping_interval",
+            "activate_scheduler",
+            "auto_clean",
+        )
