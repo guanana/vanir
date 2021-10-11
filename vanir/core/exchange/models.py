c5fb201429d9f0b9edd24e92ad1ec7d0eaebc0bb
guanana <guanana2@gmail.com>
2021-10-11 01:34:41 +0100

    Add AllowedPairs model to keep in the DB the price and history of the pair price. A lot of problems in implementation so it's left just to have a ref but not used anywhere for now

--- vanir/core/exchange/models.py
+++ vanir/core/exchange/models.py
@@ -1,3 +1,4 @@
+from bulk_update_or_create import BulkUpdateOrCreateQuerySet
 from django.db import models
 
 from vanir.core.blockchain.models import Blockchain
@@ -13,3 +14,29 @@ class Exchange(BaseObject):
     default_blockchain = models.ForeignKey(
         Blockchain, on_delete=models.CASCADE, null=True
     )
+
+
+class AllowedPairs(models.Model):
+    """
+    TODO: Check how if this is possible in future
+    Pretend to use it for get a general table with history of price but
+    multiple challenges to overcome, like speed (BulkUpdateOrCreateQuerySet) helped
+    there and history (HistoricalRecords) not being updated because BulkUpdateOrCreateQuerySet not
+    sending signal. Not used for now, left here for ref
+    """
+
+    source = models.CharField(max_length=40, default="Binance")
+    pair = models.CharField(max_length=15, unique=True)
+    price = models.DecimalField(max_digits=30, decimal_places=8, null=True)
+    # history = HistoricalRecords()
+    objects = BulkUpdateOrCreateQuerySet.as_manager()
+
+    # Make it abstract for now to now have the tables created
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.pair
+
+    def natural_key(self):
+        return (self.pair,)
