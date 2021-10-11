a9a51a1ad23b75d06aa2f2b3eb485df262dbdb72
guanana <guanana2@gmail.com>
2021-10-11 01:28:43 +0100

    Add constants for help on forms

--- vanir/order/constants.py
+++ vanir/order/constants.py
@@ -2,3 +2,5 @@ STOPPRICE = "STOP_LOSS and TAKE_PROFIT will execute a MARKET order when the stop
 PRICE = "Price at which you want to buy/sell"
 TIME_IN_FORCE = "Time in force indicates how long your order will remain active before it is executed or expired"
 ORDER_TAKE_STOP = "STOP_LOSS and TAKE_PROFIT will execute a MARKET order when the stopPrice is reached"
+TOKEN_FROM = "Token hold in your account that you want to sell with the pair created from token to"
+ACCOUNT = "Select an account to populate automatically the tokens on Token from"
