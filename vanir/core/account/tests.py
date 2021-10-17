3fc6bc894a584c4034bd4bcf2a5f73096f60d6c1
guanana <guanana2@gmail.com>
2021-10-17 03:56:11 +0100

    Fix tests

--- vanir/core/account/tests.py
+++ vanir/core/account/tests.py
@@ -1,23 +1,20 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from vanir.core.account.models import Account
-from vanir.core.blockchain.models import Blockchain
 from vanir.core.exchange.models import Exchange
 from vanir.core.token.models import Token
-from vanir.users.admin import User
 
 
 class AccountModelTest(TestCase):
     def setUp(self):
-        self.blockchain = Blockchain.objects.create(name="TestBlockchain")
-        self.token = Token.objects.create(
-            name="Test", symbol="TST", blockchain=self.blockchain
-        )
+        self.token = Token.objects.create(name="Test", symbol="TST")
         self.exchange = Exchange.objects.create(
             name="exchange_test", native_token=self.token
         )
         self.user = User.objects.create(username="user_test")
         self.account = Account.objects.create(
+            name="account1",
             exchange=self.exchange,
             user=self.user,
             api_key="account1_1234",
@@ -32,6 +29,7 @@ class AccountModelTest(TestCase):
 
     def test_defaultaccount(self):
         self.account2 = Account.objects.create(
+            name="account2",
             exchange=self.exchange,
             user=self.user,
             api_key="account2_1234",
