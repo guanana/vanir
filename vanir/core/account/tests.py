0c8acd7a9a9ab7fd1af77b67a7f5ebc277e1ccb4
guanana <guanana2@gmail.com>
2021-11-07 18:19:05 +0000

    Add tests for views

--- vanir/core/account/tests.py
+++ vanir/core/account/tests.py
@@ -2,6 +2,7 @@ from unittest.mock import patch
 
 import pytest
 from binance.exceptions import BinanceAPIException
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.urls import resolve, reverse
 
@@ -146,3 +147,58 @@ class TestAccountUrls(TestCase):
     def test_more_account_url(self):
         path = self.url_test("more", {"pk": 1})
         assert resolve(path).view_name == "account:account_more"
+
+
+class AccountViewTestNoAuth(TestCase):
+    def setUp(self):
+        number_of_accounts = 12
+        self.token = Token.objects.create(name="Test", symbol="TST")
+        self.exchange = Exchange.objects.create(
+            name="exchange_test", native_token=self.token
+        )
+        for account_id in range(number_of_accounts):
+            aux_create_basic_account(name=f"account{account_id}", exchange=self.exchange, token_pair=self.token)
+
+    def test_view_url_no_auth(self):
+        url = "/account/"
+        response = self.client.get(url)
+        login = "/users/login/?next="
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(f"{login}{url}", f"{login}{response.request['PATH_INFO']}")
+
+    def test_view_url_accessible_by_pk_no_auth(self):
+        pk = Account.objects.get(name="account0").pk
+        url = reverse('account:account_detail', args=[pk])
+        response = self.client.get(url)
+        login = "/users/login/?next="
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(f"{login}{url}", f"{login}{response.request['PATH_INFO']}")
+
+
+class AccountViewTestAuth(TestCase):
+    def setUp(self):
+        number_of_accounts = 12
+        self.token = Token.objects.create(name="Test", symbol="TST")
+        self.exchange = Exchange.objects.create(
+            name="exchange_test", native_token=self.token
+        )
+        for account_id in range(number_of_accounts):
+            aux_create_basic_account(name=f"account{account_id}", exchange=self.exchange, token_pair=self.token)
+        self.admin_user = User.objects.create_superuser('admin', 'admin@test.com', "admin")
+
+    def test_view_url_auth(self):
+        self.client.logout()
+        url = "/account/"
+        login = self.client.login(username=self.admin_user.username, password='admin')
+        response = self.client.get(url)
+        self.assertEqual(login, True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_pk_auth(self):
+        self.client.logout()
+        pk = Account.objects.get(name="account0").pk
+        url = reverse('account:account_detail', args=[pk])
+        login = self.client.login(username=self.admin_user.username, password='admin')
+        response = self.client.get(url)
+        self.assertEqual(login, True)
+        self.assertEqual(response.status_code, 200)
