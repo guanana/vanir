49c423f70fe71147acd5e9b6b0872b9147e55d97
guanana <guanana2@gmail.com>
2021-11-08 03:37:00 +0000

    Add Fiat convertor and tests

--- vanir/utils/datasource/tests.py
+++ vanir/utils/datasource/tests.py
@@ -1,4 +1,5 @@
-from unittest.mock import patch
+import json
+from unittest.mock import PropertyMock, patch
 
 from django.test import TestCase
 from pycoingecko import CoinGeckoAPI
@@ -6,6 +7,7 @@ from pycoingecko import CoinGeckoAPI
 from vanir.core.account.helpers.test_helpers import aux_create_basic_account
 from vanir.core.exchange.models import Exchange
 from vanir.core.token.models import Token
+from vanir.utils.datasource.fiat_convertor import FiatConvertorVanir
 
 ping_dict = {"gecko_says": "(V3) To the Moon!"}
 all_coins_list = [
@@ -139,3 +141,47 @@ class TestAccountHelpers(TestCase):
         self.assertEqual(mock_pycoingecko_all_coins_list.call_count, 1)
         self.assertEqual(mock_get_price.call_count, 1)
         self.assertEqual(mock_supported_fiat_list.called, False)
+
+
+exchange_rate_dict = {"result": "success",
+                      "provider": "https://www.exchangerate-api.com",
+                      "documentation": "https://www.exchangerate-api.com/docs/free",
+                      "terms_of_use": "https://www.exchangerate-api.com/terms",
+                      "time_last_update_unix": 1636329751,
+                      "time_last_update_utc": "Mon, 08 Nov 2021 00:02:31 +0000",
+                      'time_next_update_unix': 1636416461,
+                      'time_next_update_utc': 'Tue, 09 Nov 2021 00:07:41 +0000',
+                      'time_eol_unix': 0,
+                      'base_code': 'USD',
+                      'rates': {'USD': 1,
+                                'AUD': 1.35,
+                                'BMD': 1,
+                                'BSD': 1,
+                                'BTN': 74.46,
+                                'EUR': 0.865,
+                                'GBP': 0.741,
+                                'NZD': 1.41,
+                                'RSD': 101.98,
+                                'RUB': 71.35
+                                }
+                      }
+
+
+class TestFiatConvertor(TestCase):
+    def test_api_get(self):
+        with patch('requests.get') as mock_request:
+            url = "https://open.er-api.com/v6/latest/USD" # noqa F841
+            fiatconverter = FiatConvertorVanir()
+            type(mock_request.return_value).content = PropertyMock(return_value=json.dumps(exchange_rate_dict))
+            rates = fiatconverter.rates()
+            self.assertEqual(rates, exchange_rate_dict["rates"])
+            self.assertEqual(mock_request.called, True)
+
+    def test_convert_fiat(self):
+        with patch('requests.get') as mock_request:
+            url = "https://open.er-api.com/v6/latest/USD" # noqa F841
+            fiatconverter = FiatConvertorVanir()
+            type(mock_request.return_value).content = PropertyMock(return_value=json.dumps(exchange_rate_dict))
+            usd_to_eur = fiatconverter.convert_from("EUR", 1)
+            self.assertEqual(usd_to_eur, 0.865)
+            self.assertEqual(mock_request.called, True)
